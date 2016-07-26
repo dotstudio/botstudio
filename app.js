887678a0239c6bab4eaf6e96164595f9e4a9333c
n0bisuke <rsksount@gmail.com>
2016-07-27 01:15:49 +0900

    only @dotstud-io reply

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Gitter = require('node-gitter');
 const gitter = new Gitter(process.env.TOKEN);
 const util = require('util');
 
+const BOT_NAME = 'dotstud-io'; //bot名
 const ROOM_NAME = 'dotstudio/botstudio'; //todo 複数指定したい
 const connect_rooms = require('./lib/connect_rooms'); //roomへの接続
 
@@ -17,7 +18,9 @@ const COMMANDS = {
 function msgRouter(msg){
   console.log('Message: ' + msg);
   let core = {msg: JSON.parse(msg),gitter: gitter, ROOM_NAME: ROOM_NAME};
-  if(!util.isObject(core.msg.mentions[0])) return;
+  if(!util.isObject(core.msg.mentions[0])) return; // リプライがなかったらスルー
+  if(core.msg.mentions[0].screenName !== BOT_NAME) return; //ボット名に対してのリプじゃなかったらスルー
+  console.log(core.msg);
   let command_name = core.msg.text.split(' ')[1];
   if(command_name !== '') COMMANDS[command_name](core);
 }
