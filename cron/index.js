d0cae37a05cb48c24a9bdbd3e09c67d861182d88
n0bisuke <rsksound@gmail.com>
2016-07-28 06:59:38 +0900

    日報チェッカー

---
+++ cron/index.js
@@ -0,0 +1,29 @@
+/**
+ * 定期実行スクリプト
+ */
+
+'use strict'
+
+let CronJob = require('cron').CronJob;
+let sendMes = require('../lib/post_message');
+
+module.exports = (gitter) => {    
+    //日報催促
+    new CronJob('00 15 10 * * 1-5', () => {
+        gitter.rooms.join('dotstudio/ds-bot')
+        .then((room) => {
+            room.send('@/all Nippoを出しましょう！');
+        }); 
+    }, () => {},true,'Asia/Tokyo');
+
+    //ゴミ出し催促
+    new CronJob('00 55 18 * * 0,3', () => {
+        gitter.rooms.join('dotstudio/botstudio')
+        .then((room) => {
+            room.send('@/all 明日は燃えるゴミの日ですよ！');
+        }); 
+    }, () => {},true,'Asia/Tokyo');
+
+    
+}
+
