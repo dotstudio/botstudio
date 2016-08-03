aae423f14abd5ade6f4b1a4df40c04eb5159964a
のびすけ <rsksound@gmail.com>
2016-08-03 19:23:41 +0900

    deploy

--- commons/deploy.js
+++ commons/deploy.js
@@ -6,9 +6,9 @@ let mes = '';
 module.exports = (command, cb) => {
     exec(command, (error, stdout, stderr) => {
         if (error != null) {
-            mes += 'デプロイに失敗しました!\n'+'```\n'+error+'\n```';
+            mes += 'デプロイに失敗しました!\n'+'`'+error+'`';
         }else{
-            mes += 'デプロイ成功!\n'+'```\n'+stdout+'\n```';
+            mes += 'デプロイ成功!\n'+'`'+stdout+'`';
         }
 
         cb(mes);
