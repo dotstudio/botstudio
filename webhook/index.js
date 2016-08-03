061606db55072ada52fdef2c614deb1a241a0660
のびすけ <rsksound@gmail.com>
2016-08-04 04:52:10 +0900

    debug deploy

--- webhook/index.js
+++ webhook/index.js
@@ -41,10 +41,10 @@ function _selfReload(name) {
         }
 
         pm2.restart({name: name}, (err, apps) => {
+            console.log('再起動します。\n\n');
             pm2.disconnect();   // Disconnect from PM2
             if (err) throw err;
-            console.log(apps);
-            console.log('再起動します。\n\n');
+            // console.log(apps);
         });
     });
 }
