3d9235493cbc81e14b9cbdf450ba738ef8e195f2
のびすけ <rsksound@gmail.com>
2016-08-04 04:46:56 +0900

    debug deploy

--- webhook/index.js
+++ webhook/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const deploy = require('../commons/deploy');
+const pm2 = require('pm2');
 
 module.exports = (gitter) => {
     require('./lib/server')(webhook);
@@ -19,6 +20,7 @@ module.exports = (gitter) => {
                     console.log(mes);
                     // room.send(mes);
                     // room.send('デプロイ完了。 Botが進化しました。');
+                    _selfReload('dsbot'); //botプログラムの再起動
                 })
             });
 
@@ -27,4 +29,21 @@ module.exports = (gitter) => {
         }
     }
 
+}
+
+//自身を再起動
+function _selfReload(name) {
+    pm2.connect((err) => {
+
+        if (err) {
+            console.error(err);
+            process.exit(2);
+        }
+
+        pm2.restart({name: name}, (err, apps) => {
+            pm2.disconnect();   // Disconnect from PM2
+            if (err) throw err;
+            // console.log(apps);
+        });
+    });
 }
