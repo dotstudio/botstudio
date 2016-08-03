ebdcf8b572a1c2eb576354b9c3f43489b0ab475a
のびすけ <rsksound@gmail.com>
2016-08-03 17:51:32 +0900

    deployのエラーハンドリング

--- commons/deploy.js
+++ commons/deploy.js
@@ -1,10 +1,17 @@
 'use strict'
 
-const execSync = require('child_process').execSync;
-
+// const execSync = require('child_process').execSync;
+const execSpawn = require('child_process').spawnSync;
 module.exports = ()=> {
     let command = "cd web-test && git pull origin master && git push special master";
-    let result = "" + execSync(command);
-    console.log(result);
-    return result;
+    let result = execSpawn(command, {encoding:'utf8'});
+    let mes = '';
+    if(result.error || result.stderr){        
+        console.log('エラーあり');
+        mes += "" + result.stderr + result.error;
+    }else{
+        console.log('成功');
+        mes += "" + result.stdout;
+    }
+    return mes;
 }
