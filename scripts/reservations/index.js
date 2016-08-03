ba044c58f36bd35c5e075ffe96248562a41b6fff
のびすけ <rsksound@gmail.com>
2016-08-04 03:20:48 +0900

    add lib/storage

--- scripts/reservations/index.js
+++ scripts/reservations/index.js
@@ -2,25 +2,26 @@
 
 //@dotstud-io reservations add 006_8/2_08:00
 
-let sendMes = require('../../lib/post_message');
-let reservations = [];
+const sendMes = require('../../lib/post_message');
+const DB = require('../../lib/storage'); //ストレージ
+DB.init();
 
 module.exports = (roomId,msg) => {
-    let sendText = 'hey';
+    let sendText = '';
     let sendUser = msg.fromUser.username;
     let tmp = msg.text.split(' ');
     console.log(tmp);
     if(sendUser !== 'n0bisuke'){
-        sendText = `君には${tmp[1]}権限がないよ`;
+        sendText += `君には${tmp[1]}権限がないよ`;
     }
 
     let subCmd = tmp[2];
     if(subCmd === 'add'){
-        sendText = add(tmp[3]);
+        sendText += add(tmp[3]);
     }else if(subCmd === 'list'){
-        sendText = list();
+        sendText += list();
     }else{
-        sendText = 'ハハッ';
+        sendText += 'ハハッ';
     }
 
     // sendMes(roomId, sendText).then((body) => {console.log(body);});
@@ -33,17 +34,14 @@ function add(options) {
    let date = tmp[1];
    let time = tmp[2];
    let mes = `${postId}を${date}の${time}に予約しました。`;
-   console.log(mes);
-
-   reservations.push(mes);
-
+   DB.addReservations(mes);
    return mes;
 }
 
 //予約を表示
 function list() {
     let mes = '予約一覧\n';
-    mes += reservations.toString();
+    mes += DB.listReservations();
     console.log(mes);
     return mes;
 }
