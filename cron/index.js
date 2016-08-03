6a26afd17ad8105b5e4042c6955fdd5a0a63452b
のびすけ <rsksound@gmail.com>
2016-08-03 18:06:17 +0900

    deply test

--- cron/index.js
+++ cron/index.js
@@ -26,7 +26,7 @@ module.exports = (gitter) => {
     }, () => {},true,TIME_ZONE);
 
     //deploy: testサーバにあるものをpullしてdeploy
-    new CronJob('00 00 17 * * 1-5', () => {
+    new CronJob('00 07 18 * * 1-5', () => {
         console.log('デプロイスタート...');
         gitter.rooms.join('dotstudio/ds-bot')
         .then((room) => {
